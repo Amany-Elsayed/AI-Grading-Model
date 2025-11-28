a27b18f5fe794f2c5df01ecc49df5699765d0a26
Amany-Elsayed <amanyelsayed0@outlook.com>
2025-11-28 11:14:28 +0200

    added the ability to score multipul exams

--- grading_model.py
+++ grading_model.py
@@ -38,54 +38,67 @@ def grade_exam(questions, student_answers):
         else:
             earned = 0 #لو السؤال مش فقرة هيتم تجاهله
 
-        results[q_id] = round(earned, 2)
+        results[q_id] = earned
         total += earned
 
-    return results, round(total, 2), max_total
+    return results, total, max_total
 
-def load_exam_file(path):
+def load_json(path): #بفتح الفايلات
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
     
 
-def load_student_file(path): 
-    with open(path, "r", encoding="utf-8") as f:
-        return json.load(f)
-    
+def save_results(exam_name, student_name, results, total, max_total): #بكريت ملفات الدرجات
 
-def save_results(student_name, results, total, max_total):
+    os.makedirs(f"data/results/{exam_name}", exist_ok=True)
+    path = f"data/results/{exam_name}/{student_name}_results.json"
+
     output_data = {
+        "exam": exam_name,
         "student": student_name,
         "scores": results,
         "total": total,
         "max_total": max_total
     }
 
-    os.makedirs("results", exist_ok=True)
-    path = f"data/results/{student_name}_results.json"
-
     with open(path, "w", encoding="utf-8") as f:
         json.dump(output_data, f, ensure_ascii=False, indent=4)
 
-    print(f"Saved results to {path}")
+    print(f"Saved results for {student_name} in {path}")
+
+
+def process_exam(exam_file): #هنا بشوف الامتحان واخذ اسمه و اقارنه بالملف الي بنفس الاسم و اشوف الطلاب فيه
+    exam_name = exam_file.replace(".json", "")
+    exam_path = f"data/exams/{exam_file}"
+
+    print(f"\n processing exam: {exam_name}")
+
+    exam_data = load_json(exam_path)
+
+    students_folder = f"data/students/{exam_name}"
+    if not os.path.exists(students_folder):
+        print(f"No students folder found for this exam: {students_folder}")
+        return
     
+    for student_file in os.listdir(students_folder):
+        if student_file.endswith(".json"):
+            student_name = student_file.replace(".json", "")
+            student_path = os.path.join(students_folder, student_file)
+
+            print(f"scoring student: {student_name}")
+
+            student_answers = load_json(student_path)
+
+            results, total, max_total = grade_exam(exam_data, student_answers)
+
+            save_results(exam_name, student_name, results, total, max_total)
+
+            print(f"score: {total} / {max_total}")
+
+
 if __name__ == "__main__":
+    exams_folder = "data/exams/"
 
-    exam_data = load_exam_file("data/exams/exam.json")
-    
-    STUDENT_FOLDER = "data/students/"
-
-    for filename in os.listdir(STUDENT_FOLDER):
-        if filename.endswith(".json"):
-            student_name = filename.replace(".json", "")
-            file_path = os.path.join(STUDENT_FOLDER, filename)
-
-            print(f"\n Scoring student: {student_name}")
-
-            student_data = load_student_file(file_path)
-
-            results, total, max_total = grade_exam(exam_data, student_data)
-
-            save_results(student_name, results, total, max_total)
-
-            print(f"→ Total score for {student_name}: {total} / {max_total}")
+    for exam_file in os.listdir(exams_folder):
+        if exam_file.endswith(".json"):
+            process_exam(exam_file)
